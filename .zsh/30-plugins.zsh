d5513f241b4ce32921fee4036cadb9a8e5bfca63
Tomas Gavenciak <gavento@gmail.com>
2024-11-03 00:11:05 +0100

    Update zsh config

---
+++ .zsh/30-plugins.zsh
@@ -0,0 +1,5 @@
+### Import functions and other plugins
+
+source $ZSH_DIR/submodules/zsh-autosuggestions/zsh-autosuggestions.zsh
+# source $ZSH_DIR/submodules/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+source $ZSH_DIR/submodules/fzf-tab/fzf-tab.plugin.zsh
