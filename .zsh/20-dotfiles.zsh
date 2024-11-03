4d6f01e1fcd73f90283f48ac0dc9f0fc67089206
Tomas Gavenciak <gavento@gmail.com>
2024-11-03 02:40:20 +0100

    Add alt+j JQ builder, install jq

--- .zsh/20-dotfiles.zsh
+++ .zsh/20-dotfiles.zsh
@@ -3,7 +3,7 @@
 alias dot-git="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 
 function dot-install() {
-    chsh -s $(which zsh)
-    sudo apt update && sudo apt install -y bat mosh lsd ripgrep tmux fzf tldr mc htop
+    sudo chsh -s $(which zsh) $USER
+    sudo apt update && sudo apt install -y bat mosh lsd ripgrep tmux fzf tldr mc htop jq
 
 }
