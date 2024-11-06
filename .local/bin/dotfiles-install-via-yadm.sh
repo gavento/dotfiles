e80a25534be72588568bb9a71aecb19f6481fd84
Tomas Gavenciak <gavento@gmail.com>
2024-11-06 19:30:53 +0100

    Update inst scripts

--- .local/bin/dotfiles-install-via-yadm.sh
+++ .local/bin/dotfiles-install-via-yadm.sh
@@ -4,6 +4,7 @@ set -e -u -o pipefail
 TGT_PATH=$HOME/.local/bin/yadm
 if ! command -v yadm &> /dev/null && ! [[ -f $TGT_PATH ]]; then
     echo "$TGT_PATH not found, installing"
+    mkdir -p $HOME/.local/bin
     curl -fLo $TGT_PATH https://raw.githubusercontent.com/yadm-dev/yadm/refs/tags/3.2.2/yadm
     echo "dd6a9a9f6442a1b8c742952132413c68ce55c077c5fd49f85a3aa219a33b6198" $TGT_PATH | sha256sum -c || \
         ( echo Wrong SHA on $TGT_PATH !!!; rm $TGT_PATH; false )
@@ -11,6 +12,6 @@ if ! command -v yadm &> /dev/null && ! [[ -f $TGT_PATH ]]; then
 fi
 PATH=$HOME/.local/bin:$PATH
 
-yadm clone https://github.com/gavento/dotfiles
+yadm clone https://github.com/gavento/dotfiles -f
 
-dotfiles-install-fzf.sh
+$HOME/.local/bin/dotfiles-install-fzf.sh
