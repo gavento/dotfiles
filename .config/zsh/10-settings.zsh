38475612fa170692f7fc8440320fd35467644fbc
Tomas Gavenciak <gavento@gmail.com>
2024-11-06 17:37:47 +0100

    Minor updates

--- .config/zsh/10-settings.zsh
+++ .config/zsh/10-settings.zsh
@@ -16,7 +16,7 @@ HISTSIZE=100000
 SAVEHIST=100000
 HISTDUP=erase
 setopt append_history
-setopt share_history
+#setopt share_history
 setopt inc_append_history
 setopt hist_ignore_all_dups
 setopt hist_save_no_dups
