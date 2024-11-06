e4ba54fa778f2e0a1fbef622d093a15d06017ffa
Tomas Gavenciak <gavento@gmail.com>
2024-11-06 16:40:00 +0100

    Add zim downloaded modules

---
+++ .config/zimfw/init.zsh
@@ -0,0 +1,9 @@
+# FILE AUTOMATICALLY GENERATED FROM /home/gavento/.zimrc
+# EDIT THE SOURCE FILE AND THEN RUN zimfw build. DO NOT DIRECTLY EDIT THIS FILE!
+
+if [[ -e ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]] zimfw() { source "${HOME}/.config/zimfw/zimfw.zsh" "${@}" }
+source "${HOME}/.config/zimfw/modules/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "${HOME}/.config/zimfw/modules/fzf/init.zsh"
+source "${HOME}/.config/zimfw/modules/zsh-z/zsh-z.plugin.zsh"
+source "${HOME}/.config/zimfw/modules/completion/init.zsh"
+source "${HOME}/.config/zimfw/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
