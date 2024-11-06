63460351358cdd3b8189962f29af999fc9200a12
Tomas Gavenciak <gavento@gmail.com>
2024-11-06 13:04:34 +0100

    Update zsh config (WIP)

--- .zsh/20-dotfiles.zsh
+++
@@ -1,9 +0,0 @@
-### Settingf for managing the dot files
-
-alias dot-git="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
-
-function dot-install() {
-    sudo chsh -s $(which zsh) $USER
-    sudo apt update && sudo apt install -y bat mosh lsd ripgrep tmux fzf tldr mc htop jq
-
-}
---
+++ .zsh/50-plugins.zsh
@@ -0,0 +1,6 @@
+### Import concrete plugins
+
+source $ZSH_DIR/submodules/zsh-autosuggestions/zsh-autosuggestions.zsh
+#source $ZSH_DIR/submodules/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+#source $ZSH_DIR/submodules/fzf-tab/fzf-tab.plugin.zsh
+#source $ZSH_DIR/submodules/jq-zsh-plugin/jq.plugin.zsh
