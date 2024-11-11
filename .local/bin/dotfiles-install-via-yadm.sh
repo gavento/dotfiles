4f652ba10b704e997086e2c5b565865bece7207f
Tomas Gavenciak <gavento@gmail.com>
2024-11-11 13:51:15 +0100

    Minor updates - debugging

--- .local/bin/dotfiles-install-via-yadm.sh
+++ .local/bin/dotfiles-install-via-yadm.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e -u -o pipefail
 
+# Installs yadm if not present (system or .local/bin)
+# Clones dotfiles with YADM if not present already
+# Installs a newer fzf locally if not present
+
 TGT_PATH=$HOME/.local/bin/yadm
 if ! command -v yadm &> /dev/null && ! [[ -f $TGT_PATH ]]; then
     echo "$TGT_PATH not found, installing"
@@ -10,8 +14,17 @@ if ! command -v yadm &> /dev/null && ! [[ -f $TGT_PATH ]]; then
         ( echo Wrong SHA on $TGT_PATH !!!; rm $TGT_PATH; false )
     chmod a+x $TGT_PATH
 fi
+
 PATH=$HOME/.local/bin:$PATH
 
-yadm clone https://github.com/gavento/dotfiles -f
+# If this is a fresh install, initialize yadm
+if [ ! -d $HOME/.local/share/yadm ]; then
+  cd $HOME
+  yadm clone https://github.com/gavento/dotfiles
+  pwd
+  echo $HOME
+  ls -l $HOME/.local/
+  ls -l $HOME/.local/bin/
+fi
 
-$HOME/.local/bin/dotfiles-install-fzf.sh
+$HOME/.local/bin/dotfiles-install-fzf.sh || true
