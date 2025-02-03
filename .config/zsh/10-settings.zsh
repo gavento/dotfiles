e5d7506653d186cbd1ca3d77b2579c9fff9c8f4e
Tomas Gavenciak <gavento@gmail.com>
2025-02-03 01:30:58 +0100

    Update config, fixes

--- .config/zsh/10-settings.zsh
+++ .config/zsh/10-settings.zsh
@@ -14,7 +14,7 @@ PROMPT='%F{green}%n@%m%f %F{blue}%1~%f %# '
 local functions_dir=$HOME/.config/zsh/functions
 FPATH=$functions_dir:$HOME/.local/share/zsh/functions:$FPATH
 
-local functions_list=($functions_dir/^[._]*(N:t))
+local functions_list=($functions_dir/[^._]*(N:t))
 if [[ -n $functions_list ]] then
     autoload -U $functions_list
 fi
@@ -53,19 +53,24 @@ alias zim-fw="source $ZIM_HOME/zimfw.zsh"
 
 ### ZSH configuration
 
+# Only match tab completion on the preceding characters
+bindkey '\CI' expand-or-complete-prefix
+
+setopt globdots
+
 # ignore case
-# zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-# # disable sort when completing `git checkout`
-# zstyle ':completion:*:git-checkout:*' sort false
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
 # # set descriptions format to enable group support
 # # NOTE: don't use escape sequences here, fzf-tab will ignore them
 # zstyle ':completion:*:descriptions' format '[%d]'
-# # set list-colors to enable filename colorizing
-# zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 # # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
 # zstyle ':completion:*' menu no
-# # preview directory's content with eza when completing cd
-# zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+# preview directory's content with eza when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
 # # switch group using `<` and `>`
 # zstyle ':fzf-tab:*' switch-group '<' '>'
 
